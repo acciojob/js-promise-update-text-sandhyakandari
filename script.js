22e215052557721f8fd06c58ded897743c7c1801
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-27 17:28:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let prom=new Promise(function(resolve,reject){
+setTimeout(function(){
+	resolve("Hello, world")},1000)
+	
+})
+prom.then(function(value){
+	 let div = document.getElementById("output");
+	  div.innerHTML=value;
+})
